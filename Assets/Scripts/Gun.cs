7b06dbb1ec346b6b99d7127b6501cd42e2653bf3
Jean Sucre <jeansucre88@yahoo.co.uk>
2017-10-26 16:08:44 -0300

    Minor tweaks

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -8,6 +8,7 @@ public class Gun : MonoBehaviour {
 	public int magazineSize;
 	public float pistolCooldown;
 	public float mgCooldown;
+	public float shotgunCooldown;
 
 	private int maxMagazineSize;
 	private int mode = 1;
@@ -30,6 +31,9 @@ public class Gun : MonoBehaviour {
 		else  if (Input.GetKeyDown (KeyCode.Alpha2)) {
 			mode = 2;
 		}
+		else  if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			mode = 3;
+		}
 
 		if (Input.GetMouseButton (0) && cooldownTimer <= 0) {
 			if (mode == 1) {
@@ -40,6 +44,9 @@ public class Gun : MonoBehaviour {
 			} else if (mode == 2) {
 				FireGun ();
 				cooldownTimer = mgCooldown;
+			} else if (mode == 3) {
+				FireShotgun ();
+				cooldownTimer = shotgunCooldown;
 			}
 		}
 		if (Input.GetKeyDown (KeyCode.R)) {
@@ -51,6 +58,16 @@ public class Gun : MonoBehaviour {
 		if (magazineSize > 0) {
 			magazine.Find ();
 			magazineSize--;
+		}
+	}
+
+	void FireShotgun()
+	{
+		if (magazineSize >= 5) {
+			for (int i = 0; i < 5; i++) {
+				magazine.Find ();
+				magazineSize--;
+			}
 		}
 	}
 
