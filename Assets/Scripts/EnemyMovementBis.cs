f8c673607b3495c56d573b4ffc1df1f84e23e4e4
Jean Sucre <jeansucre88@yahoo.co.uk>
2017-10-31 00:37:08 -0300

    Better movement

--- Assets/Scripts/EnemyMovementBis.cs
+++ Assets/Scripts/EnemyMovementBis.cs
@@ -1,33 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EnemyMovementBis : MonoBehaviour {
 
 	public Vector2 startPos;
+	public Vector2 midPos;
 	public Vector2 endPos;
+
+	public bool parabola = false;
+	public float time;
 	private float horizontalSpeed;
 	private float verticalSpeed;
-	public float topY;
-	public float time;
-	private float toppY;
+	private float topY;
+	private float toppYa;
+	private float toppYB;
+	private float time1;
+	private float time2;
 
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3(startPos.x, startPos.y,  0);
-		Vector2 temp = startPos - endPos;
-		Vector2 temp2 = endPos - startPos;
-		topY = topY - transform.position.y;
+		transform.position = new Vector3 (startPos.x, startPos.y, 0);
+		Vector2 temp =  endPos - startPos;
+		if (parabola) {
+			Vector2 temp3 = midPos - startPos;
+			Vector2 temp4 = endPos - midPos;
+			time1 = (time * temp3.x) / temp.x;
+			time2 = (time * temp4.x) / temp.x;
+			verticalSpeed = (temp3.y / time1) * 2;
+			toppYa = ((temp3.y / time1) * 2) / time1;
+			toppYB = ((temp4.y / time2) * 2) / time2;
+		} else if (!parabola) {
+			verticalSpeed = temp.y / time;
+		}
+		
 		horizontalSpeed = temp.x / time;
-		verticalSpeed = (topY / time) * 4;
-		toppY = (verticalSpeed / (time / 2));
+
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += Vector3.left * horizontalSpeed * Time.deltaTime;
+		transform.position += Vector3.right * horizontalSpeed * Time.deltaTime;
 		transform.position += Vector3.up * verticalSpeed * Time.deltaTime;
-		verticalSpeed -= toppY * Time.deltaTime;
-		Debug.Log(Time.timeSinceLevelLoad);
+		if (parabola) {
+			verticalSpeed -= toppYa * Time.deltaTime;
+			if (Vector2.Distance (transform.position, midPos) <= 1) {
+				toppYa = -toppYB;
+			}
+		}
+		Debug.Log (Time.timeSinceLevelLoad);
 	}
 }
