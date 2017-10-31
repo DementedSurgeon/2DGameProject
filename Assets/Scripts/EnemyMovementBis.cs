4a04758c9971239337847668af9f50d9bce45768
Jean Sucre <jeansucre88@yahoo.co.uk>
2017-10-31 16:52:01 -0300

    Enemy Movement Bis

--- Assets/Scripts/EnemyMovementBis.cs
+++ Assets/Scripts/EnemyMovementBis.cs
@@ -10,6 +10,8 @@ public class EnemyMovementBis : MonoBehaviour {
 	public Vector2 endPos;
 
 	public bool parabola = false;
+	public bool reverseX = false;
+	public bool reverseY = false;
 	public float time;
 	private float horizontalSpeed;
 	private float verticalSpeed;
@@ -36,20 +38,80 @@ public class EnemyMovementBis : MonoBehaviour {
 		}
 		
 		horizontalSpeed = temp.x / time;
+		if (reverseX) {
+			ReverseX ();
+		}
 
+		if (reverseY) {
+			ReverseY ();
+		}
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
+		
 		transform.position += Vector3.right * horizontalSpeed * Time.deltaTime;
 		transform.position += Vector3.up * verticalSpeed * Time.deltaTime;
 		if (parabola) {
 			verticalSpeed -= toppYa * Time.deltaTime;
-			if (Vector2.Distance (transform.position, midPos) <= 1) {
+			if (Vector2.Distance (transform.position, midPos) <= (2/time)) {
 				toppYa = -toppYB;
 			}
 		}
-		Debug.Log (Time.timeSinceLevelLoad);
+
+
+	}
+
+	public void ReverseX()
+	{
+		startPos = new Vector2 (startPos.x * -1, startPos.y);
+		endPos = new Vector2 (endPos.x * -1, endPos.y);
+		midPos = new Vector2 (midPos.x * -1, midPos.y);
+		transform.position = new Vector3 (startPos.x, startPos.y, 0);
+		Vector2 temp =  endPos - startPos;
+		if (parabola) {
+			Vector2 temp3 = midPos - startPos;
+			Vector2 temp4 = endPos - midPos;
+			time1 = (time * temp3.x) / temp.x;
+			time2 = (time * temp4.x) / temp.x;
+			verticalSpeed = (temp3.y / time1) * 2;
+			toppYa = ((temp3.y / time1) * 2) / time1;
+			toppYB = ((temp4.y / time2) * 2) / time2;
+		} else if (!parabola) {
+			verticalSpeed = temp.y / time;
+		}
+
+		horizontalSpeed = temp.x / time;
+
+	}
+
+	public void ReverseY()
+	{
+		startPos = new Vector2 (startPos.x, startPos.y * -1);
+		endPos = new Vector2 (endPos.x, endPos.y* -1);
+		midPos = new Vector2 (midPos.x, midPos.y* -1);
+		transform.position = new Vector3 (startPos.x, startPos.y, 0);
+		Vector2 temp =  endPos - startPos;
+		if (parabola) {
+			Vector2 temp3 = midPos - startPos;
+			Vector2 temp4 = endPos - midPos;
+			time1 = (time * temp3.x) / temp.x;
+			time2 = (time * temp4.x) / temp.x;
+			verticalSpeed = (temp3.y / time1) * 2;
+			toppYa = ((temp3.y / time1) * 2) / time1;
+			toppYB = ((temp4.y / time2) * 2) / time2;
+		} else if (!parabola) {
+			verticalSpeed = temp.y / time;
+		}
+
+		horizontalSpeed = temp.x / time;
+
+	}
+
+	public void Reset()
+	{
+		transform.position = new Vector3 (startPos.x, startPos.y, 0);
+		gameObject.SetActive (false);
 	}
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
-	public GameObject prefab;
+	public GameObject[] prefab;
 	public int spawnCount;
 	public Transform target;
 	public ProjPool enemyProjPool;
 
 	private float timer = 0;
 	public float timerDelay = 0.5f;
+	private float globalTimer = 0;
+	public float globalTimerDelay;
 	private bool spawning = false;
-
+	private int totalSpawn;
+	private int counter;
 	public EnemyLook[] badDudes; 
 
 
@@ -21,18 +24,24 @@ public class Spawner : MonoBehaviour {
 		if (spawnCount <= 0) {
 			spawnCount = 1;
 		}
-		badDudes = new EnemyLook[spawnCount];
-		for (int i = 0; i < badDudes.Length; i++) {
-			badDudes [i] = Instantiate (prefab, transform.position, Quaternion.identity, transform).GetComponentInChildren<EnemyLook> ();
-			badDudes [i].SetTarget (target);
-			badDudes [i].gameObject.GetComponent<Arsenal> ().SetAmmoPool (enemyProjPool);
+		badDudes = new EnemyLook[spawnCount * prefab.Length];
+		for (int s = 0; s < prefab.Length; s++) {
+			for (int i = s*spawnCount; i < (spawnCount + s*spawnCount); i++) {
+				badDudes [i] = Instantiate (prefab[s], transform.position, Quaternion.identity, transform).GetComponentInChildren<EnemyLook> ();
+				badDudes [i].SetTarget (target);
+				badDudes [i].gameObject.GetComponent<Arsenal> ().SetAmmoPool (enemyProjPool);
+			}
 		}
+
+		totalSpawn = spawnCount * prefab.Length;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (globalTimer > 0) {
+			globalTimer -= Time.deltaTime;
+		}
 		if (timer > 0) {
 			timer -= Time.deltaTime;
 		}
@@ -40,14 +49,21 @@ public class Spawner : MonoBehaviour {
 			spawning = true;
 			}
 		if (spawning) {
-			if (timer <= 0) {
-				spawnCount--;
-				badDudes [spawnCount].transform.parent.gameObject.SetActive (true);
-				timer = timerDelay;
-				//Debug.Log (spawnCount);
-			}
-			if (spawnCount == 0) {
-				spawning = false;
+			if (globalTimer <= 0) {
+				if (timer <= 0) {
+					totalSpawn--;
+					counter++;
+					badDudes [totalSpawn].transform.parent.gameObject.SetActive (true);
+					timer = timerDelay;
+					if (counter == spawnCount) {
+						globalTimer = globalTimerDelay;
+						counter = 0;
+					}
+					Debug.Log (globalTimer);
+				}
+				if (totalSpawn == 0) {
+					spawning = false;
+				}
 			}
 		}
 	}
