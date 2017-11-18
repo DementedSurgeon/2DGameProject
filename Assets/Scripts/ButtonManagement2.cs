b08f775d01c4aa79ab7ab03ccdc15e42f988f497
DementedSurgeon <30392632+DementedSurgeon@users.noreply.github.com>
2017-11-17 21:11:03 -0300

    New scenes

---
+++ Assets/Scripts/ButtonManagement2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManagement2 : MonoBehaviour {
+
+	public Button startButton;
+	public Button quitButton;
+
+	// Use this for initialization
+	void Start () {
+		startButton.onClick.AddListener(delegate() {
+			SceneManager.LoadScene("TitleScreen");
+		});
+		quitButton.onClick.AddListener(Application.Quit);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
