9c74b4f3fa771a87b53826252e3276a05a19fbaa
DementedSurgeon <30392632+DementedSurgeon@users.noreply.github.com>
2017-11-17 21:08:58 -0300

    Organization

--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -1,30 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameFlow : MonoBehaviour {
 
+	private float timer;
+	public float timerDelay;
+
 	public Health health;
 	public BossSpawner boss;
+	public delegate void MyDelegate();
+	public MyDelegate StartSpawn;
 
 	// Use this for initialization
 	void Start () {
 		health.Death += GameLost;
 		boss.OnBossDead += GameWon;
+		timer = timerDelay;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (timer > 0) {
+			timer -= Time.deltaTime;
+			if (timer <= 0)
+			{
+				if (StartSpawn != null) {
+					StartSpawn ();
+				}
+
+			}
+		}
 	}
 
 	void GameWon()
 	{
-		Debug.Log ("Game won!");
+		SceneManager.LoadScene ("YouWin");
 	}
 
 	void GameLost()
 	{
-		Debug.Log ("Game lost!");
+		SceneManager.LoadScene ("YouLose");
 	}
 }
